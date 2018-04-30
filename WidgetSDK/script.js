b95e456835220e6e03c47ebe77bd2596571e3395
sivanh <sivanh@liveperson.com>
2018-04-30 13:37:16 +0300

    create widget SDK to github

---
+++ WidgetSDK/script.js
@@ -0,0 +1,39 @@
+new Vue({
+    el: '#app',
+    data: {
+        agentMessagesValue:0,
+        consumerMessagesValue:0
+    },
+    methods: {
+        updateData: function(data){
+            debugger;
+            if(data && data.newValue){
+                var i;
+                for(i = 0; i < data.newValue.length; i++){
+                    if(data.newValue[i].source === "agent") {
+                        this.agentMessagesValue++;
+                    }
+                    else if(data.newValue[i].source === "visitor") {
+                        this.consumerMessagesValue++;
+                    }
+                }
+            }
+        },
+        onSendNotificationClicked: function() {
+            alert("Submit");
+        }
+
+    },
+    computed: {
+        getQueryParamsDetails() {
+            const paramsString = location.search;
+            const searchParams = new URLSearchParams(paramsString);
+            const result = [...searchParams];
+            return result;
+        }
+    },
+    created(){
+        lpTag.agentSDK.init({});
+        lpTag.agentSDK.bind("chatTranscript.lines", this.updateData);
+    }
+});
